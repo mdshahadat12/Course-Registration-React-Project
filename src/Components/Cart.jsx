e27624ed19dda7490f7561c10f321b318c581773
Md Shahadat <mdsadt2@gmail.com>
2023-09-15 07:13:33 +0600

    total price, total credit, remaining credit done

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-const Cart = ({data}) => {
+const Cart = ({data,ramain,credit,total}) => {
     // console.log(data);
     // const {title} = data;
     return (
         <div className="lg:w-1/4 p-5 bg-white rounded-lg">
             <div className="lg:sticky top-8 right-0">
-                <h1 className="text-lg text-[#2F80ED] font-bold my-4">Credit Hour Remaining 7 hr</h1>
+                <h1 className="text-lg text-[#2F80ED] font-bold my-4">Credit Hour Remaining {ramain} hr</h1>
                 <hr />
                 <h1 className="text-xl text-[#1C1B1B] font-bold">Course Name</h1>
                 <ol className="list-decimal m-5 text-[#1C1B1B99]">
@@ -14,9 +14,9 @@ const Cart = ({data}) => {
                 }
                 </ol>
                 <hr />
-                <p className="font-medium my-3">Total Credit Hour : 13</p>
+                <p className="font-medium my-3">Total Credit Hour : {credit}</p>
                 <hr />
-                <p className="font-medium my-3">Total Price : 48000 USD</p>
+                <p className="font-medium my-3">Total Price : {total} USD</p>
             </div>
         </div>
     );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,8 +5,34 @@ import Header from "./header";
 
 const Home = () => {
     const [data, setdata] = useState([]);
+    const [credit, setCredit] = useState(0)
+    const [ramain, setRamain] = useState(20)
+    const [total, setTotal] = useState(0)
+
     const handleBtn = (cards) => {
-        setdata([...data,cards]);
+        const exsist = data.find(element => cards.id === element.id);
+        let total = cards.price;
+        data.forEach(element => {
+            total += element.price;
+        });
+        
+        let credit = cards.credit;
+        data.forEach(element => {
+            credit += element.credit;
+        });
+        const ramain = 20 - credit
+        if(ramain < 0){
+            return alert('limit ses')
+        }else 
+        if(exsist){
+            return alert('booked');
+        }else{
+            
+            setTotal(total);
+            setCredit(credit);
+            setRamain(ramain);
+            setdata([...data,cards]);
+        }
     }
     return (
         <div className="bg-[#F3F3F3]">
@@ -14,7 +40,7 @@ const Home = () => {
                 <Header></Header>
                 <div className="flex flex-col-reverse lg:flex-row gap-10">
                 <Cards handleBtn={handleBtn}></Cards>
-                <Cart data={data}></Cart>
+                <Cart data={data} total={total} credit={credit} ramain={ramain}></Cart>
                 </div>
              </div>
         </div>
